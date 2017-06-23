787cd7a01abb9ad1493490c3368bb0ece77c5037
Matthew Vukomanovic <matthew.vukomanovic@gmail.com>
2017-06-23 21:43:47 +0800

    Create shared project to make different versions

---
+++ Vukomanovic.Resharper.Macros.2016.2/buildpackage-debug.bat
@@ -0,0 +1,2 @@
+nuget pack Vukomanovic.Resharper.Macros.Debug.nuspec -OutputDirectory ..\built -NoPackageAnalysis
+pause
--- Vukomanovic.Resharper.Macros.2017.1.Test/Properties/AssemblyInfo.cs
+++ Vukomanovic.Resharper.Macros.2017.1.Test/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Vukomanovic.Resharper.Macros")]
+[assembly: AssemblyTitle("Vukomanovic.Resharper.Macros.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vukomanovic.Resharper.Macros")]
-[assembly: AssemblyCopyright("Copyright © Matthew Vukomanovic 2016")]
+[assembly: AssemblyProduct("Vukomanovic.Resharper.Macros.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a9720913-e8e7-4040-bfdc-d0fc2fd86cb9")]
+[assembly: Guid("d8dfb461-9a4a-47b2-865a-20d2e42ec4a2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.2017.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
