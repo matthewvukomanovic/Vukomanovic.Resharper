4eb7d334622260af03a1f5059ffebda7b8f94e25
Matthew Vukomanovic <matthew.vukomanovic@gmail.com>
2017-06-23 21:51:30 +0800

    Include constants for shared

---
+++ Vukomanovic.Resharper.Macros.Shared/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vukomanovic.Resharper.Macros.Shared
+{
+    public static class Constants
+    {
+        public const string Version = "1.1";
+    }
+}
