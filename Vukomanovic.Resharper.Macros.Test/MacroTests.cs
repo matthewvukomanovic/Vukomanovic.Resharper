e7f8aadbc94126a08f5eeb0610a0a723fdc2a804
Matthew Vukomanovic <matthew.vukomanovic@gmail.com>
2017-06-23 16:44:01 +0800

    Make tests more extensive

--- Vukomanovic.Resharper.Macros.Test/MacroTests.cs
+++ Vukomanovic.Resharper.Macros.Test/MacroTests.cs
@@ -19,31 +19,62 @@ namespace Vukomanovic.Resharper.Macros.Test
         }
 
         [Theory]
-        [InlineData("_preSomething_post", "l", "something")]
-        [InlineData("_preSomething_post", "u", "Something")]
-        [InlineData("_preSomething_post", "L", "something")]
-        [InlineData("_preSomething_post", "U", "SOMETHING")]
-        [InlineData("_preSomething_post", "", "Something")]
-        [InlineData("O", "", "O")]
-        [InlineData("_post", "", "")]
-        [InlineData("_pre", "", "")]
-        [InlineData("_pre_post", "", "")]
-        [InlineData("_pRE_poSt", "", "")]
-        [InlineData("_pre_pre", "", "_pre")]
-        [InlineData("_post_post", "", "_post")]
-        [InlineData("_prepost", "", "post")]
-        [InlineData("pre_post", "", "pre")]
-        [InlineData("_pre_pre", "", "_pre")]
-        [InlineData("_ppre_post", "", "_ppre")]
-        [InlineData("_pre_postp", "", "_postp")]
-        [InlineData("_ppre_pre_postp", "", "_ppre_pre_postp")]
-        public void RemovePreSuffixVariable(string initial, string transform, string expected)
+        [InlineData("Hello", "_", "_", "l", "_hello_")]
+        [InlineData("HelloBirdy", "_", "_", "l", "_helloBirdy_")]
+        [InlineData("HelloBirdy", "_", "", "l", "_helloBirdy")]
+        [InlineData("hello", "_", "_", "l", "_hello_")]
+        [InlineData("helloBirdy", "_", "_", "l", "_helloBirdy_")]
+        [InlineData("helloBirdy", "_", "", "l", "_helloBirdy")]
+        [InlineData("Hello", "_", "_", "u", "_Hello_")]
+        [InlineData("HelloBirdy", "_", "_", "u", "_HelloBirdy_")]
+        [InlineData("HelloBirdy", "_", "", "u", "_HelloBirdy")]
+        [InlineData("hello", "Pre", "_", "u", "PreHello_")]
+        [InlineData("helloBirdy", "Pre", "_", "u", "PreHelloBirdy_")]
+        [InlineData("helloBirdy", "Pre", "", "u", "PreHelloBirdy")]
+        public void AddPreSuffixVariable(string initial, string prefix, string suffix, string transform, string expected)
+        {
+            var c = new MacroParameterValueCollection
+            {
+                new DelegateMacroParameter(() => initial),
+                new ConstantMacroParameterValue(prefix),
+                new ConstantMacroParameterValue(suffix),
+                new DelegateMacroParameter(() => transform),
+            };
+
+
+            IMacroImplementation i = new AddPreSufFixVariableMacroImplementation(c);
+            var value = i.EvaluateQuickResult(null);
+
+            Xunit.Assert.Equal(expected, value);
+        }
+
+
+        [Theory]
+        [InlineData("_preSomething_post", "_pre", "_post", "l", "something")]
+        [InlineData("_preSomething_post", "_pre", "_post", "u", "Something")]
+        [InlineData("_preSomething_post", "_pre", "_post", "L", "something")]
+        [InlineData("_preSomething_post", "_pre", "_post", "U", "SOMETHING")]
+        [InlineData("_preSomething_post", "_pre", "_post", "", "Something")]
+        [InlineData("O", "_pre", "_post", "", "O")]
+        [InlineData("_post", "_pre", "_post", "", "")]
+        [InlineData("_pre", "_pre", "_post", "", "")]
+        [InlineData("_pre_post", "_pre", "_post", "", "")]
+        [InlineData("_pRE_poSt", "_pre", "_post", "", "")]
+        [InlineData("_pre_pre", "_pre", "_post", "", "_pre")]
+        [InlineData("_post_post", "_pre", "_post", "", "_post")]
+        [InlineData("_prepost", "_pre", "_post", "", "post")]
+        [InlineData("pre_post", "_pre", "_post", "", "pre")]
+        [InlineData("_pre_pre", "_pre", "_post", "", "_pre")]
+        [InlineData("_ppre_post", "_pre", "_post", "", "_ppre")]
+        [InlineData("_pre_postp", "_pre", "_post", "", "_postp")]
+        [InlineData("_ppre_pre_postp", "_pre", "_post", "", "_ppre_pre_postp")]
+        public void RemovePreSuffixVariable(string initial, string pre, string post, string transform, string expected)
         {
             var c = new MacroParameterValueCollection
                 {
                     new DelegateMacroParameter(() => initial),
-                    new ConstantMacroParameterValue("_pre"),
-                    new ConstantMacroParameterValue("_post"),
+                    new ConstantMacroParameterValue(pre),
+                    new ConstantMacroParameterValue(post),
                     new DelegateMacroParameter(() => transform),
                 };
 
